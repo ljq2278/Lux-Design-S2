111eee3433c91e9c0ee2e8e45169a50026797126
ljq2278 <ljq2278@163.com>
2023-05-05 21:36:48 +0800

    levels

---
+++ kits/rl/my_rl/actcrt_model/ac_attention_model.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+from torch.optim import Adam
+from torch.distributions import Categorical
+from wrappers.obs_space_levels import ObsSpaceUnit
+import numpy as np
+
+
+class ActMLPNetwork(nn.Module):
+    def __init__(self, in_dim, out_dim, env_cfg, num_bins=1100, embedding_dim=8, mn=-1, mx=300, hidden_dim=64, non_linear=nn.Tanh()):
+        super(ActMLPNetwork, self).__init__()
+        self.obsSpaceUnit = ObsSpaceUnit(env_cfg)
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.embedding_dim = embedding_dim
+        self.num_bins = num_bins
+        self.mn = mn
+        self.mx = mx
+        self.embeddings = nn.ModuleList([nn.Embedding(num_bins, embedding_dim) for _ in range(in_dim)])
+        self.attn_net = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=6)
+        self.linear = nn.Linear(in_dim * embedding_dim, out_dim)
+        self.sfmx = nn.Softmax(dim=-1)
+
+    def discretize(self, x):
+        bins = torch.linspace(self.mn, self.mx, self.num_bins)
+        x = x.permute(1, 0).contiguous()
+        binned = torch.stack([torch.bucketize(x[i, :], bins) for i in range(x.shape[0])], dim=1)
+        return binned
+
+    def forward(self, x):
+        x = torch.clamp(x, self.mn, self.mx)
+        x_discrete = self.discretize(x)
+        x_embedded = torch.stack([self.embeddings[i](x_discrete[:, i]) for i in range(x.shape[1])], dim=1)
+        atte_out, _ = self.attn_net(x_embedded, x_embedded, x_embedded)
+        atte_out_rs = atte_out.reshape([-1, self.embedding_dim * self.in_dim])
+        ret = self.linear(atte_out_rs)
+        ret_prob = self.sfmx(ret)
+        return ret_prob
+
+
+class CriMLPNetwork(nn.Module):
+    def __init__(self, in_dim, out_dim, env_cfg, num_bins=1100, embedding_dim=8, mn=-1, mx=300, hidden_dim=64, non_linear=nn.Tanh()):
+        super(CriMLPNetwork, self).__init__()
+        self.obsSpaceUnit = ObsSpaceUnit(env_cfg)
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.embedding_dim = embedding_dim
+        self.num_bins = num_bins
+        self.mn = mn
+        self.mx = mx
+        self.embeddings = nn.ModuleList([nn.Embedding(num_bins, embedding_dim) for _ in range(in_dim)])
+        self.attn_net = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=1)
+        self.linear = nn.Linear(in_dim * embedding_dim, 1)
+
+    def discretize(self, x):
+        bins = torch.linspace(self.mn, self.mx, self.num_bins)
+        x = x.permute(1, 0).contiguous()
+        binned = torch.stack([torch.bucketize(x[i, :], bins) for i in range(x.shape[0])], dim=1)
+        return binned
+
+    def forward(self, x):
+        x = torch.clamp(x, self.mn, self.mx)
+        x_discrete = self.discretize(x)
+        x_embedded = torch.stack([self.embeddings[i](x_discrete[:, i]) for i in range(x.shape[1])], dim=1)
+        atte_out, _ = self.attn_net(x_embedded, x_embedded, x_embedded)
+        atte_out_rs = atte_out.reshape([-1, self.embedding_dim * self.in_dim])
+        ret = self.linear(atte_out_rs)
+        return ret
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, env_cfg, hidden_dim=64, non_linear=nn.ReLU()):
+        super(ActorCritic, self).__init__()
+        self.actor = ActMLPNetwork(state_dim, action_dim, env_cfg)
+        self.critic = CriMLPNetwork(state_dim, action_dim, env_cfg)
+
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state):
+        state = torch.FloatTensor(state)
+        action_probs = self.actor(state)
+        dist = Categorical(action_probs)
+        action = dist.sample()
+        action_logprob = dist.log_prob(action)
+        state_val = self.critic(state)
+        return action.tolist(), action_logprob.tolist(), state_val.tolist()
+
+    def evaluate(self, state, action):
+        action_probs = self.actor(state)
+        dist = Categorical(action_probs)
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        state_values = self.critic(state)
+        return action_logprobs, state_values, dist_entropy
---
+++ kits/rl/my_rl/ppo/UnitAgentAtten.py
@@ -0,0 +1,72 @@
+from copy import deepcopy
+from typing import List
+import os
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+from torch.optim import Adam
+from torch.distributions import Categorical
+import numpy as np
+
+from actcrt_model.ac_attention_model import ActorCritic
+from ppo.UnitBuffer import Buffer
+
+
+
+class PPO_Online_Agent:
+    def __init__(self, state_dim, action_dim, env_cfg):
+        self.policy = ActorCritic(state_dim, action_dim, env_cfg)
+
+    def update(self, new_params):
+        self.policy.load_state_dict(new_params)
+
+
+class PPO_Offline_Agent:
+    def __init__(self, state_dim, action_dim, env_cfg, lr_actor, lr_critic, eps_clip, save_dir='./'):
+        self.eps_clip = eps_clip
+        self.policy = ActorCritic(state_dim, action_dim, env_cfg)
+        self.optimizer = torch.optim.Adam([
+            {'params': self.policy.actor.parameters(), 'lr': lr_actor},
+            {'params': self.policy.critic.parameters(), 'lr': lr_critic}
+        ])
+        self.save_dir = save_dir
+        self.mseLoss = nn.MSELoss()
+
+    def update_and_get_new_param(self, train_data, K_epochs):
+        # Monte Carlo estimate of returns
+        # Optimize policy for K epochs
+        for _ in range(K_epochs):
+            for pid_data in train_data:
+                # Evaluating old actions and values
+                old_states, old_actions, old_logprobs, old_state_vals, old_rewards, old_done, advantages = \
+                    [torch.Tensor(np.array(x)) for x in pid_data]
+                logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+                # match state_values tensor dimensions with rewards tensor
+                state_values = torch.squeeze(state_values)
+                # Finding the ratio (pi_theta / pi_theta__old)
+                ratios = torch.exp(logprobs - old_logprobs)
+                # Finding Surrogate Loss
+                surr1 = ratios * advantages
+                surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
+                # final loss of clipped objective PPO
+                loss = -torch.min(surr1, surr2) + 0.5 * self.mseLoss(state_values, old_rewards) - 0.01 * dist_entropy
+                # take gradient step
+                self.optimizer.zero_grad()
+                loss.mean().backward()
+                self.optimizer.step()
+
+        return self.policy.state_dict()
+
+    def save(self):
+        """save actor parameters of all agents and training reward to `res_dir`"""
+        torch.save({
+            'policy': self.policy.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+        }, os.path.join(self.save_dir, 'model.pt'))
+
+    def load(self):
+        """init maddpg using the model saved in `file`"""
+        print('loading model .................')
+        checkpoint = torch.load(os.path.join(self.save_dir, 'model.pt'))
+        self.policy.load_state_dict(checkpoint['policy'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
