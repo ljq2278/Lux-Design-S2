1e63d64063f6bd606f19b13807ab5cc11a0db29d
jiaqing.lin <ljq2278@163.com>
2023-05-22 20:58:37 +0800

    bug fix

--- kits/rl/my_rl/wrappers/rwd_transfer.py
+++ kits/rl/my_rl/wrappers/rwd_transfer.py
@@ -50,8 +50,8 @@ class RwdTransfer:
             'power_generation': (stats['generation']['power']['FACTORY'] - last_stats['generation']['power']['FACTORY']) * 0,
             'water_generation': stats['generation']['water'] - last_stats['generation']['water'],
             'metal_generation': stats['generation']['metal'] - last_stats['generation']['metal'],
-            'ice_transfer': stats['transfer']['ice'] - last_stats['transfer']['ice'],
-            'ore_transfer': stats['transfer']['ore'] - last_stats['transfer']['ore'],
+            # 'ice_transfer': stats['transfer']['ice'] - last_stats['transfer']['ice'],
+            # 'ore_transfer': stats['transfer']['ore'] - last_stats['transfer']['ore'],
             'destroyed_rubble': (sum(list(stats['destroyed']['rubble'].values())) - sum(list(last_stats['destroyed']['rubble'].values()))) / 10,
         }
         # if done:
